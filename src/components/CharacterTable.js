0de33d60bb0d58dd73631bac2a952f3c5852cad9
Akos Oravecz <oraveczakos2001@gmail.com>
2024-03-24 17:11:07 +0100

    implement search

--- src/components/CharacterTable.js
+++ src/components/CharacterTable.js
@@ -1,29 +1,41 @@
 import { useEffect, useState } from "react";
 
-import { Image, Table } from "react-bootstrap"
+import { Image, InputGroup, Form, Table } from "react-bootstrap"
 
 import getCharacters from "../graphql/getCharacters";
 
 export default function CharacterTable({ currentPage, handleViewChar }) {
   const [ characters, setCharacters ] = useState([]);
   const [ loading, setLoading ] = useState(false);
+  const [ search, setSearch ] = useState("");
 
   useEffect(() => {
     const loadChars = async () => {
       setLoading(true);
 
-      const data = await getCharacters(currentPage);
+      const data = await getCharacters(currentPage, search);
       setCharacters(data?.characters?.results);
 
       setLoading(false);
     };
 
     loadChars();
-  }, [currentPage]);
+  }, [currentPage, search]);
+  
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  }
 
   return <>
     <h1>Home</h1>
+    <InputGroup className="mb-3">
+      <InputGroup.Text id="basic-addon3">
+        Search
+      </InputGroup.Text>
+      <Form.Control id="basic-url" aria-describedby="basic-addon3" onChange={handleSearch} />
+    </InputGroup>
     {loading ? "Loading..." :
+    characters.length === 0 ? <h2>Character not found.</h2> :
     <Table hover>
         <thead>
         <tr>
--- src/graphql/getCharacters.js
+++ src/graphql/getCharacters.js
@@ -1,10 +1,10 @@
 import gqlClient from './client.js';
 import { gql } from 'graphql-request';
 
-export default async function getCharacters(page=1) {
+export default async function getCharacters(page=1, search="") {
     const data = await gqlClient.request(gql`
     query {
-        characters(page: ${page}) {
+        characters(${search === "" ? `page: ${page}` : `filter: { name: "${search}" }`}) {
         results {
                 id
                 image
